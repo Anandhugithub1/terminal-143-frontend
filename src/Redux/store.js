3a02d3cbdb2ecb87179b9d70d639023f848e1694
Anandhugithub1 <122972305+Anandhugithub1@users.noreply.github.com>
2025-05-14 09:34:38 +0530

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,12 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Auth/slice';
 import profileReducer from './Profile/slice';
-import userProfileReducer from './User/slice'; 
-
+import userProfileReducer from './User/profile-slice'; 
+import updateProfileReducer from './User/updateSlice'; // Assuming you have this slice
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     profiles: profileReducer,
     userprofile: userProfileReducer,
+    profileUpdate: updateProfileReducer
   }
 });
