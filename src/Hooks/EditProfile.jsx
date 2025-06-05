6a834549ec8f3a7b3c21386a8afa8af08e8aaea5
Anandhugithub1 <anantuashokan333@gmail.com>
2025-06-05 12:15:31 +0530

    intail upadte of Hooks

--- src/Hooks/EditProfile.jsx
+++ src/Hooks/EditProfile.jsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchProfile, selectProfile, selectProfileStatus } from '../Redux/User/profile-slice';
-import { updateProfileData, uploadProfileImage } from '../Redux/User/updateSlice';
+import { fetchProfile } from '../features/UserProfile';
+import { updateProfileData, uploadProfileImage } from '../features/UserProfile';
 
 export function useEditableProfile() {
   const dispatch = useDispatch();
-  const profile = useSelector(selectProfile);
-  const status = useSelector(selectProfileStatus);
+  // Inline selectors instead of importing a separate "selectProfile" file
+  const profile = useSelector((state) => state.userProfile.currentUser);
+  const status = useSelector((state) => state.userProfile.status);
+
   const [localAvatar, setLocalAvatar] = useState('');
   const [localBio, setLocalBio] = useState('');
 
   useEffect(() => {
-    if (status === 'idle') dispatch(fetchProfile());
+    if (status === 'idle') {
+      dispatch(fetchProfile());
+    }
   }, [dispatch, status]);
 
   useEffect(() => {
-    if (status === 'succeeded') {
+    if (status === 'succeeded' && profile) {
       setLocalAvatar(profile.profilePhoto || profile.photo || '');
       setLocalBio(profile.bio || '');
     }
@@ -36,7 +40,6 @@ export function useEditableProfile() {
     if (updated !== null) {
       setLocalBio(updated);
       dispatch(updateProfileData({ bio: updated }));
-      
     }
   };
 
