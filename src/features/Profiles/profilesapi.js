7031bcec3d90ef399acfc58fc91439fb01eb0dfa
Anandhugithub1 <anantuashokan333@gmail.com>
2025-06-05 10:47:02 +0530

    updated api url

---
+++ src/features/Profiles/profilesapi.js
@@ -0,0 +1,17 @@
+/* ================= features/profiles/api.js ================= */
+import axios from 'axios';
+
+const BASE_URL = 'https://userapi.terminal143.com/';
+
+
+export const getMatchProviders = async ({ preferences, limit = 10, }) => {
+  const response = await axios.get(
+    `${BASE_URL}/match-providers/all`,
+    
+    {
+      params: { limit, preferences: JSON.stringify(preferences) },
+      withCredentials: true,
+    }
+  );
+  return response.data.items || [];
+};
