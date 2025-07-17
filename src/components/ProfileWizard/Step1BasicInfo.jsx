c9f174d004d7ccd4b5fc64dfd7ad1ff3ca3b2050
Anandhugithub1 <anantuashokan333@gmail.com>
2025-07-17 21:19:40 +0530

    added social media upload option

--- src/components/ProfileWizard/Step1BasicInfo.jsx
+++ src/components/ProfileWizard/Step1BasicInfo.jsx
@@ -189,7 +189,7 @@ const Step1BasicInfo = () => {
         type="text"
         value={socialInput}
         onChange={e => setSocialInput(e.target.value)}
-        placeholder="Username or https://link"
+        placeholder="Username or Profile Link"
         className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
       />
     </div>
