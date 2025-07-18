2138e6a30278567c46adcf1edfe09bf5297a7b2b
Anandhugithub1 <anantuashokan333@gmail.com>
2025-07-18 15:35:23 +0530

    updated imports

--- src/Utlis/utlis.js
+++ src/Utlis/utlis.js
@@ -98,6 +98,18 @@ export const interestMap = {
 };
 
 
+
+   export function calculateAge(dob) {
+  const birthDate = new Date(dob);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
 export function computeAge(dob) {
   if (!dob) return '';
   const birth = new Date(dob);
--- src/pages/User/Request.jsx
+++ src/pages/User/Request.jsx
@@ -5,19 +5,7 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import TopNav from '../../components/Layout/TopNavigation';
 import BottomNav from '../../components/Layout/BottomNavigation';
 import { ConfirmationModal } from '../../components/Ui/Confirmation';
-
-
-function calculateAge(dob) {
-  const birthDate = new Date(dob);
-  const today = new Date();
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  return age;
-}
-
+import {calculateAge} from '../../Utlis/utlis';
 
 
 export default function RequestsPage() {
