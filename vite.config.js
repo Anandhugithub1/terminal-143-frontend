223e9e59cfd9e4758aef28b9cace56a8d0396888
Anandhugithub1 <anantuashokan333@gmail.com>
2025-07-23 12:12:07 +0530

    chore(vite): update configuration for better performance added manual code splitting

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-
+  plugins: [
+    react(),
     tailwindcss()
-
   ],
-  
+
+  build: {
+    // bump the warning threshold if you like:
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // anything from node_modules → vendor.js
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
