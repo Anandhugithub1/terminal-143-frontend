6075d2043f258bbbaed47e980c11e88312e2298f
Anandhugithub1 <anantuashokan333@gmail.com>
2025-11-13 20:17:52 +0530

    updated spacing

--- src/features/UserProfile/components/PublicProfile/TopBar.jsx
+++ src/features/UserProfile/components/PublicProfile/TopBar.jsx
@@ -7,7 +7,7 @@ export default function PublicTopbar() {
 
   return (
     <div className="w-full bg-white border-b border-gray-100">
-      <div className="max-w-2xl mx-auto px-4 py-3 flex items-center justify-between">
+      <div className="max-w-2xl mx-auto px-3 py-2 flex items-center justify-between">
         
         {/* Logo */}
         <div className="flex items-center space-x-2">
