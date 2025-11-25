d9635421aa0749d892465ed975887e5700bbb4f6
Anandhugithub1 <anantuashokan333@gmail.com>
2025-11-25 22:28:43 +0530

    added location api client

--- src/api/clients.js
+++ src/api/clients.js
@@ -5,8 +5,10 @@ const isDev = import.meta.env.VITE_MODE === 'dev';
 
 const userApiBase = isDev ? import.meta.env.VITE_API_USER : 'https://userapi.terminal143.com';
 const matchesApiBase = isDev ? import.meta.env.VITE_API_MATCH : 'https://userapi.terminal143.com/match';
+const LocationApiBase = 'https://authapi.terminal143.com/location'
 const authApiBase = isDev ? import.meta.env.VITE_API_AUTH : 'https://authapi.terminal143.com';
 
 export const userProfilesApi = createApiClient(userApiBase);
 export const matchesApi = createApiClient(matchesApiBase);
 export const authApi = createApiClient(authApiBase);
+export const locationAPi =createApiClient(LocationApiBase);
