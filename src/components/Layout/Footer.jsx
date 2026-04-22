b7932afcc8fa08b0c040e71e019eb15149af1ea4
Anandhugithub1 <anantuashokan333@gmail.com>
2026-04-22 13:35:46 +0530

    updated nav links

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -33,7 +33,7 @@ export default function Footer() {
 
     if (link === 'faq') {
       return (
-        <a href="/help/faq" className={className}> 
+        <a href="/help/faq.html" className={className}> 
           {label}
         </a>
       )
